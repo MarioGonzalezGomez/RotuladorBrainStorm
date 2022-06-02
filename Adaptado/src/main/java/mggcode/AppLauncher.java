0cd4fe372b99e1ab639153cba0ad2fed219192f4
unknown <Administrador@P715389.rtve.int>
2022-06-02 08:52:07 +0200

    Preparando Instalador

---
+++ Adaptado/src/main/java/mggcode/AppLauncher.java
@@ -0,0 +1,10 @@
+package mggcode;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class AppLauncher {
+    public static void main(String[] args) throws SQLException, FileNotFoundException {
+        Main.main(args);
+    }
+}
